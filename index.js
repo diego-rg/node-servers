a1d0b111d1b11ddc20bde5208ee55de892f5ae2c
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-12 22:16:03 +0100

    response render html css

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import http from "http";
 import fs from "fs";//Necesario para ler o html
+import path from "path"
 
-const server = http.createServer((req, res) =>{
-    res.writeHead(200, { "Content-Type": "text/html; charset=UTF-8" });
-    const file = "./index.html";
-    fs.readFile(file, (err, content) => {
-        if (err) {
-            return console.log("Ha habido un error!", err);
-        }
-        res.write(content);
-        res.end();
-    });
-});
+const server = http.createServer((request, response) => {
+  let filePath = request.url
+  if (filePath === '/') {
+    filePath = 'index.html'
+  }
+  filePath = `./${filePath}`
+  const extname = path.extname(filePath)
+  let contentType = 'text/html'
+  switch (extname) {
+    case '.css':
+      contentType = 'text/css'
+      break
+  }
+  response.writeHead(200, { 'Content-Type': `${contentType}; charset=UTF-8` })
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      return console.log(err)
+    }
+    response.write(content)
+    return response.end()
+  })
+})
 
 server.listen(3000, "localhost", err => {
     if(err) {
